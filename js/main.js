c1f88470bf7a0ee0ed6b49e6d6c31182138fafa7
Mahesh-95 <53552767+Mahesh-95@users.noreply.github.com>
2020-06-25 21:05:25 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+  jQuery(document).ready(function(){
+	  
+	"use strict"
+	$('.slider').ripples({
+	  dropRadius: 15,
+	  perturbance: 0.01,
+	
+	});
+	
+	$(".text").typed({
+		strings:["<strong>Welcome to </strong><strong class='primary'>our Website.</strong>","<strong>Have a </strong><strong class='primary'>nice Day!!</strong>"],
+		 typespeed:30,
+		 loop:true,
+	});
+	
+	$(window).scroll(function(){
+		
+		var top= $(window).scrollTop();
+		if(top>=60){
+		   $("nav").addClass('secondary');
+		 }
+		else
+			if($("nav").hasClass('secondary')){
+				$("nav").removeClass('secondary');
+				 
+			 }
+	});
+
+	$('.img-wrapper').magnificPopup({
+		delegate: 'a', 
+		type: 'image',
+		gallery: {
+			enabled: true
+		  }
+	  });
+
+	  $('.owl-carousel').owlCarousel();
+
+	
+
+});
