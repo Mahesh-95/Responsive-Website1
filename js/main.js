a8b78e8beb65705c5fc5fb041d87023a8dd4068c
Mahesh-95 <53552767+Mahesh-95@users.noreply.github.com>
2020-11-11 23:15:13 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,7 +34,22 @@
 		  }
 	  });
 
-	  $('.owl-carousel').owlCarousel();
+	  $('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    responsive:{
+		0:{
+		    items:1
+		},
+		600:{
+		    items:2
+		},
+		1000:{
+		    items:3
+		}
+	    }
+	})
 
 	
 
